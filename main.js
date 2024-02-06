9a1c34ed23a37308692bfa0271251c0db89ad404
HariharanR1899 <hariharan.r1899@gmail.com>
2024-02-06 16:15:34 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Web Developer", "Software Engineer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// $(document).ready(function () {
+//   $(".menu-toggle").click(function () {
+//     $(".menu-links").slideToggle();
+//   });
+// });
