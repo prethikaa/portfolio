566c1287788acc9d7f329291ec0ef2378cde4633
Prethikaa Madhanagopal <54131324+prethikaa@users.noreply.github.com>
2024-10-12 15:10:26 +0100

    Updated

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var typed = new Typed(".text", {
-  strings: ["Frontend Developer", "Web Developer", "Software Engineer"],
+  strings: ["Full Stack Developer", "Web Developer", "Frontend Developer", "Software Engineer"],
   typeSpeed: 100,
   backSpeed: 100,
   loop: true,
 });
+
 
 // $(document).ready(function () {
 //   $(".menu-toggle").click(function () {
